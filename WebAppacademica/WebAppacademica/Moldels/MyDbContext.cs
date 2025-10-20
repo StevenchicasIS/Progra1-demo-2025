4f9cb333713684ba2872f8b518b06c39ec571136
oscar <huezosteven402@gmail.com>
2025-10-19 22:05:22 -0600

    Creacion de la rama Wedapp y del proyecto web app academica, configuracion de la calse MyDbContext y Alumno y la configuracion de la cadena de conexion a la BD

---
+++ WebAppacademica/WebAppacademica/Moldels/MyDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace WebAppacademica.Moldels
+{
+    public class MyDbcontext  : DbContext {
+        public MyDbcontext() { }
+        public MyDbcontext(DbContextOptions<MyDbcontext> options) : base(options) { }
+        public DbSet<Alumno> Alumnos { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Alumno>().HasKey(a => a.idAlumno);
+        }
+    }
+}
